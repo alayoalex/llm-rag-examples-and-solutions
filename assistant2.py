c213b2b4dca746e27ea1d222f35509963e4cb6f5
Alexei Alayo <alexei.alayo@sivah.net>
2024-09-21 01:19:32 -0400

    fixing keyword error in dict II

--- assistant2.py
+++ assistant2.py
@@ -6,6 +6,6 @@ while True:
     user_input = input('You: ')
     convo.append({'role': 'user', 'content': user_input})
     output = ollama.chat(model='llama3.1', messages=convo)
-    response = output['content']
+    response = output['message']['content']
     print('Assistant: \n{response} \n')
     convo.append({'role': 'assistant', 'content': response})
